b8161e6deb10bccb32f0380d71b4b132260db15d
khanhtuanne <khanhtuan090606@gmail.com>
2026-09-22 13:44:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Ứng dụng AI của riêng tôi 🤖")
 
 # Lấy chìa khóa API đã được giấu kín
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 # Khung nhập câu hỏi
 user_input = st.text_input("Bạn muốn hỏi gì?")
